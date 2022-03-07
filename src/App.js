c23c90bc3fc802699be53bd3357360bf50fb8668
joél <joel@joel.fm>
2022-03-07 02:23:34 -0800

    complete

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
-import React, {Component} from 'react';
-import Navigation from './components/Navigation';
-import './App.css';
+import React from 'react'
+import Navigation from './components/Navigation'
+import './App.css'
 
-const title = "HackerMaps";
+const title = 'HackerMaps'
 const locations = [
-    'Lombard St, San Francisco, CA, USA',
-    'PIER 39, The Embarcadero, San Francisco, CA, USA',
-    'Golden Gate Bridge, San Francisco, CA, USA',
-    `Fisherman's Wharf, San Francisco, CA, USA`,
-    'Alcatraz Island, San Francisco, CA, USA'
-];
+  'Lombard St, San Francisco, CA, USA',
+  'PIER 39, The Embarcadero, San Francisco, CA, USA',
+  'Golden Gate Bridge, San Francisco, CA, USA',
+  `Fisherman's Wharf, San Francisco, CA, USA`,
+  'Alcatraz Island, San Francisco, CA, USA'
+]
 
-class App extends Component {
+// class App extends Component {
+const App = () => (
+  <div className='App'>
+    <h8k-navbar header={title}></h8k-navbar>
+    <Navigation locations={locations} />
+  </div>
+)
 
-    constructor() {
-        super();
-        this.locations = [...locations];
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <h8k-navbar header={title}></h8k-navbar>
-                <Navigation locations={this.locations}/>
-            </div>
-        );
-    }
-}
-
-export default App;
+export default App
